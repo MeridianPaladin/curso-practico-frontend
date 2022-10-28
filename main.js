54ed1fe9e45acb4c4397d03935673147cab158b2
MeridianPaladin <vicmarama@hotmail.es>
2022-10-28 16:37:28 -0500

    Implementa mobile menu en navbar

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const mobileMenuicon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+mobileMenuicon.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
 }
 
+
+function toggleMobileMenu() {
+    console.log("hola")
+    mobileMenu.classList.toggle("inactive");
+}
